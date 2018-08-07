51e709abfe20e1de6328cb6217203f6bd6836fd0
Patrick Tran <patrick.tran43@gmail.com>
2018-08-06 18:20:36 -0700

    - add toggle button on demo page to turn on/off magic slider dots

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # react-magic-dots
 
 > React Magic Dots Component for React Slick Carousel
+*Inspired by Instagram*
 
 [![NPM](https://img.shields.io/npm/v/react-magic-dots.svg)](https://www.npmjs.com/package/react-magic-dots) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
 ## Install
 
 ```bash
-npm install --save react-magic-dots
+npm install --save react-magic-slider-dots
 ```
 
 ## Usage
--- example/src/App.js
+++ example/src/App.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Slider from "react-slick";
+import ToggleButton from 'react-toggle-button';
 import MagicSliderDots from 'react-magic-slider-dots';
 import 'react-magic-slider-dots/dist/react-magic-slider-dots.css';
 
 export default class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      enableMagicSliderDots: true
+    }
+  }
+
+  handleDotToggle = () => {
+    this.setState({
+      enableMagicSliderDots: !this.state.enableMagicSliderDots
+    });
+  }
+
   contentIndexes = [...Array.from({ length: 18 }, (v, k) => k + 1).map((item) => item)];
 
   render() {
+    const { enableMagicSliderDots } = this.state;
+
     var settings = {
       dots: true,
-      arrows: false,
+      arrows: true,
       infinite: false,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1,
-      appendDots: (dots) => {
-        return <MagicSliderDots dots={dots} numDotsToShow={5} dotWidth={30} />
-      }
+      slidesToScroll: 1
     };
+
+    if (enableMagicSliderDots)
+      settings['appendDots'] = (dots) => {
+        return <MagicSliderDots dots={dots} numDotsToShow={4} dotWidth={30} />
+      }
+
     return (
-      <Slider {...settings}>
-        {this.contentIndexes.map(index => <div key={index}><h3>{index}</h3></div>)}
-      </Slider>
+      <div>
+        <label><ToggleButton
+          value={enableMagicSliderDots}
+          onToggle={this.handleDotToggle} />
+          &nbsp;enable magic-slider-dots
+        </label>
+        <Slider {...settings} key={`slider_${enableMagicSliderDots}`}>
+          {this.contentIndexes.map(index => <div key={index}><h3>{index}</h3></div>)}
+        </Slider>
+      </div>
     );
   }
 }
